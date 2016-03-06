6b581b25474d90567968f536e35218007fce6e93
Aleksandar Tanchev <a.tanchev995@gmail.com>
2016-03-06 21:19:41 +0000

    Add AdmAccessToken class

---
+++ App_Code/AdmAccessToken.cs
@@ -0,0 +1,13 @@
+﻿/// <summary>
+/// Set up an AdmAccessToken class which stores the information returned from the Windows Azure Marketplace service
+/// </summary>
+public class AdmAccessToken
+{
+    public string access_token { get; set; }
+
+    public string token_type { get; set; }
+
+    public string expires_in { get; set; }
+
+    public string scope { get; set; }
+}
