ec38ab8c6e3da09dd02b85a53d1173dda8d713a6
Aleksandar Tanchev <a.tanchev995@gmail.com>
2016-03-07 21:13:55 +0100

    Add GetTranslation class

---
+++ App_Code/GetTranslation.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI.WebControls;
+
+public class GetTranslation
+{
+    // detects the language from the user input
+    public void DetectLanguage(string toDetect, DropDownList fromLanguage)
+    {
+        GetAccessToken auth = new GetAccessToken();
+        string appId = auth.GetAccessTokenString();
+        TranslatorService.LanguageServiceClient client = new TranslatorService.LanguageServiceClient();
+
+        string languageToDetect = toDetect;
+        string detectedLanguage = client.Detect(appId, languageToDetect);
+        fromLanguage.SelectedValue = detectedLanguage;
+    }
+}
