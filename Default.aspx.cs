2c3af94228b15eee7d0a35c0d46c957532531336
Aleksandar Tanchev <a.tanchev995@gmail.com>
2016-03-07 20:36:40 +0100

    Call function to fill dropdown lists with languages

--- Default.aspx.cs
+++ Default.aspx.cs
@@ -9,6 +9,7 @@ public partial class _Default : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
-
+        GetData fillList = new GetData();
+        fillList.FillLanguageList(ddlTranslateFrom, ddlTranslateTo);
     }
 }
